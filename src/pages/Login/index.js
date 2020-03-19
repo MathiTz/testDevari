3365986d00e3a17c4d13596859f788a7c2bea3e7
mathitz <matheusalves789@gmail.com>
2020-03-19 02:26:25 -0300

    Finished layout of login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderLogin from "../../components/HeaderLogin";
 
 import api from "../../services/api";
@@ -6,10 +6,45 @@ import api from "../../services/api";
 import "./style.css";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState("");
+
+  async function signUp() {}
+
   return (
     <>
       <HeaderLogin />
-      <div className="container login-page"></div>
+      <main className="container login-page">
+        <section className="login-title">Entre na sua conta</section>
+        <div className="login-form">
+          <div className="login-form__email">
+            <label htmlFor="email">E-mail</label>
+            <input
+              type="text"
+              placeholder="exemplo@exemplo.com"
+              value={username}
+              onChange={e => {
+                setUsername(e.target.value);
+              }}
+            />
+          </div>
+          <div className="login-form__password">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="********"
+              value={password}
+              onChange={e => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <div className="login-form__button">
+            <button onClick={signUp()}>Entrar</button>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
